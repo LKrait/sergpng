014e1687465923eb090e61adf858a284f4479ac1
LKrait <afiokevin@gmail.com>
2025-03-17 08:46:03 +1000

    Removing additional register.html and changing allowed host on setting file to allow any host to access site

--- sergpng/settings.py
+++ sergpng/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5omtuey#nu)qf#z8iub(d6vgscpmg)j#ylnsc9uv+()=@6a0+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
