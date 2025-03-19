198c09aa459d45b4e7443e64e4655a285d12d9e3
LKrait <afiokevin@gmail.com>
2025-03-19 20:40:19 +1000

    Last change

--- sergpng/settings.py
+++ sergpng/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'sergpng.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sergpng',
+        'USER': 'admin',
+        'PASSWORD': '!*Ty8MXa',
+        'HOST' : 'localhost',
+        'PORT': ''
     }
 }
 
